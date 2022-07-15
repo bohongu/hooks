93fe2b980654d28d80382e778ae0bc8dcaac1654
bohongu <jaehong2782@naver.com>
2022-07-15 17:13:36 +0900

    set up

---
+++ src/State.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const State = () => {
+  return <div></div>;
+};
+
+export default State;
+
+/* 
+컴포넌트의 상태를 간단하게 생성하고, 업데이트하는 훅 
+const[state.setState] = useState(초기값);
+*/
