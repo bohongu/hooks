b57a12d60805253ff4af08731ff8636676834c85
bohongu <jaehong2782@naver.com>
2022-07-17 18:12:39 +0900

    useRef finish

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import State from "./State";
 import Ref from "./Ref";
 import Ref2 from "./Ref2";
 import Ref3 from "./Ref3";
+import Ref4 from "./Ref4";
 
 function App() {
-  return <Ref3 />;
+  return <Ref4 />;
 }
 
 export default App;
